2cf4d0045594d3910a404f1ce85d8995934f6653
luska <luska@MUFASA>
2025-02-17 15:50:58 -0300

    Adicionar arquivos de projeto.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿class BubbleSort
+{
+    static void Main()
+    {
+        int[] array = { 163, 10468, 468, 5257, 58, 864, 1005, 7 };
+        int temp;
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int sort = 0; sort < array.Length - 1 - i; sort++)
+            {
+                if (array[sort] > array[sort + 1])
+                {
+                    temp = array[sort + 1];
+                    array[sort + 1] = array[sort];
+                    array[sort] = temp;
+                }
+            }
+        }
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}

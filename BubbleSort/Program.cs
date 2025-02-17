27b3e87d4152742a99ff2d79974ceaca3555db8d
Lucasmstaf <159509611+Lucasmstaf@users.noreply.github.com>
2025-02-17 20:13:06 -0300

    Update Program.cs

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -2,22 +2,22 @@
 {
     static void Main()
     {
-        int[] array = { 163, 10468, 468, 5257, 58, 864, 1005, 7 };
+        int[] arr = { 163, 10468, 468, 5257, 58, 864, 1005, 7 };
         int temp;
 
-        for (int i = 0; i < array.Length - 1; i++)
+        for (int i = 0; i < arr.Length - 1; i++)
         {
-            for (int sort = 0; sort < array.Length - 1 - i; sort++)
+            for (int sort = 0; sort < arr.Length - 1 - i; sort++)
             {
-                if (array[sort] > array[sort + 1])
+                if (arr[sort] > arr[sort + 1])
                 {
-                    temp = array[sort + 1];
-                    array[sort + 1] = array[sort];
-                    array[sort] = temp;
+                    temp = arr[sort + 1];
+                    arr[sort + 1] = arr[sort];
+                    arr[sort] = temp;
                 }
             }
         }
-        foreach (int num in array)
+        foreach (int num in arr)
         {
             Console.Write(num + " ");
         }
